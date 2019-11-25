f6a25a88dea9bdb4cad5e2bd5f441adad66a62d7
mantanquzzmo <quzzmo@hotmail.com>
2019-11-25 17:25:52 +0100

    Adds JSon projects file + modifies project constructor

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div className="ui container"> 
+      <h1 className="ui header">About Me</h1>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Projects.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react"
+import axios from "axios"
+
+class Projects extends Component {
+    constructor() {
+        super();
+        this.state = {
+          projects: []
+        };
+      }
+
+    componentDidMount() {
+        axios.get('./src/data/projects.json')
+        .then(response => {
+        this.setState({
+        projects: response.data
+        })
+    })
+}
+
+  render() {
+    const projects = this.state.projects
+    let projectsList
+
+    if (projects.length > 0) {
+      projectsList = projects.map(project => {
+        return (
+          <div key={project.id}>
+            <h3 className="ui header">
+              {project.name}
+            </h3>
+          </div>
+        )
+      })
+    }
+
+
+    return (
+      <div className="ui main container">
+        <h1 className="ui header">My Projects</h1>
+        {projectsList}
+      </div>
+    )
+  }
+};
+
+export default Projects
