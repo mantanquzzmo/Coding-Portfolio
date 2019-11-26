6ad964e39f6c43769724b1a67091fb0fd07fc12a
mantanquzzmo <quzzmo@hotmail.com>
2019-11-26 14:36:51 +0100

    Adds styling touch to card body

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -38,13 +38,14 @@ class Projects extends Component {
     return (
       <div className="ui main container">
       <div className="ui stackable two column grid">
-        <div className="column">
-          <UndrawDesigner />
-        </div>
-        <div className="column">
+      <div className="column">
           <h1 className="ui header">My Projects</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia quod ab doloremque eaque. Consequatur temporibus, quos enim, eaque nemo ad iusto sequi modi totam qui veniam? Ab asperiores inventore distinctio.</p>
         </div>
+        <div className="column">
+          <UndrawDesigner primaryColor='#12283a' height='200px' />
+        </div>
+        
       </div>
       <div className="ui stackable four column grid">
         {projectsList}
