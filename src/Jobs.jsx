4801f073afd51a18f3517d73ccf328018b1727cf
mantanquzzmo <quzzmo@hotmail.com>
2019-11-28 16:24:24 +0100

    Fixes new button with dissapearance function and styling

--- src/Jobs.jsx
+++ src/Jobs.jsx
@@ -1,87 +1,82 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-class HideText extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      textDisplay: false,
-      text: true
-    };
-  }
-
-  componentDidMount() {
-    axios.get("./src/data/jobs.json").then(response => {
-      this.setState({
-        jobs: response.data
-      });
-    });
-  }
-
-  ToggleButton() {
-    this.setState(currentState => ({
-      textDisplay: !currentState.textDisplay,
-      buttonText: !currentState.text
-    }));
-  }
-
-  divRender() {
-    return <h4>testdiv</h4>;
-  }
-
-  divRender2() {
-    return <h1>testdiv2</h1>;
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={() => this.ToggleButton()}>
-          {this.state.textDisplay ? "Experience" : "About me"}
-        </button>
-        {this.state.textDisplay ? <h4>This is my extensive experience</h4> : <h4>This is however what I do in my spare time</h4>}
-      </div>
-    );
-  }
-}
-
 class Jobs extends Component {
   constructor() {
     super();
     this.state = {
-      jobs: []
+      currentContent: null
     };
   }
 
-  componentDidMount() {
-    axios.get("./src/data/jobs.json").then(response => {
-      this.setState({
-        jobs: response.data
-      });
+  presentExperiences() {
+    return (
+      <div>
+        <p>LOOK AT ALL MY EXPERIENCES</p>
+      </div>
+    );
+  }
+
+  presentJobs() {
+    return (
+      <div>
+        <p>LOOK AT ALL MY JOBS</p>
+      </div>
+    );
+  }
+
+  displayCurrentContent() {
+    let currentContent = this.state.currentContent;
+
+    if (currentContent == 'experiences') {
+      return this.presentExperiences();
+    } else if (currentContent == 'jobs')  {
+      return this.presentJobs();
+    } else {
+      return <p>CLICK A BUTTON</p>
+    }
+
+    // return (
+    //   <div id={displayExperiences ? "experiences": "jobs"}>
+    //     {displayExperiences ? <p>Experiences</p> : <p>Jobs</p>}
+    //     <button onClick={this.handleClick.bind(this)}>
+    //       {
+    //         displayExperiences ?
+    //         "Show me jobs"
+    //         :
+    //         "Show me experiences"
+    //       }
+    //     </button>
+    //   </div>
+    // )
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentContent: event.target.id
     });
   }
 
   render() {
-    const jobs = this.state.jobs;
-    let jobsList;
-    jobsList = <div>
-    <p>Tell me about</p>
-    <HideText text="test" />
-  </div>
-    if (jobs.length > 0) {
-      jobsList = jobs.map(job => {
-        return (
-          <div>
-            <p>Tell me about</p>
-            <HideText text="test" />
-          </div>
-        );
-      });
-    }
+    let currentContent = this.state.currentContent
+    let displayCurrentContent = this.displayCurrentContent();
 
     return (
-        <div class="ui grid"><div>{jobsList}</div></div>
-        
+      <div className="ui main container">
+        {displayCurrentContent}
+        {currentContent == 'experiences' ? 
+          '' : 
+          <button id="experiences" onClick={this.handleClick.bind(this)}>
+            Show me the experiences
+          </button>
+        }
+        {currentContent == 'jobs' ? 
+          '' : 
+          <button id="jobs" onClick={this.handleClick.bind(this)}>
+            Show me the job info
+          </button>
+        }
+      </div>
     );
   }
 }
