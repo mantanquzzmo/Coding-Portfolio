d0115a76581787f392467f4fcf3ac6d364a22493
mantanquzzmo <quzzmo@hotmail.com>
2020-02-20 14:26:51 +0100

    edits project button link

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { connect } from "react-redux"
 
-const Hello = () => {
+const Hello = props => {
   return (
     <div className="ui main container">
       <div id="centered" className="ui middle aligned grid">
         <div className="row">
           <div className="column">
             <div className="header">
-              <h1 className="hello-header">Hi!</h1>
-              <div className="hello-content">I'm Martin Strid</div>
+              <h1 className="hello-header">Hey,</h1>
+              <div className="hello-content">I'm Martin Strid<br /><br />Junior Web-Developer</div>
 
               <a
                 className="ui right labeled large icon basic button"
-                href="/projects"
+                onClick={() => props.setActivities("Projects")}
               >
                 PROJECTS
                 <i className="angle double right icon"></i>
@@ -25,4 +26,14 @@ const Hello = () => {
   );
 };
 
-export default Hello;
+const mapDispatchToProps = dispatch => {
+    return {
+      setActivities: data => {
+        dispatch({ type: "SET_CURRENTVIEW", payload: data });
+      }
+    };
+  };
+  
+  export default connect(null, mapDispatchToProps)(Hello);
+
+
